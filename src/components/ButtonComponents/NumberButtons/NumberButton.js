4296f75e0e4d57024aaaa650bc80a2b72d81aa11
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2019-12-11 18:15:17 -0500

    added style to the calculator '

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -3,10 +3,12 @@ import React from "react";
 const NumberButton = (props) => {
   console.log("NumberButton", props);
   return (
-    <button className="number-btn" onClick={() => props.addNumber(props.text)}>
+
+    <button style = {{background:'blue'}} className="number-btn" onClick={() => props.addNumber(props.text) }>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.text}
     </button>
+
   );
 };
 export default NumberButton;
